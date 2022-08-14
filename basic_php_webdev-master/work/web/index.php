c77fb73fc2f9a69224082d55164b86e95766aad9
lisa <lisa.terashima72@gmail.com>
2022-08-14 16:45:56 +0900

    ドットインストール終了

--- basic_php_webdev-master/work/web/index.php
+++ basic_php_webdev-master/work/web/index.php
@@ -2,17 +2,39 @@
 
   require('../app/functions.php'); /*読み込みに失敗したときに止めてほしいからrequire */
 
+  createToken();
+
+  define('FILENAME', '../app/messages.txt');
+
+  if ($_SERVER['REQUEST_METHOD']=== 'POST') {
+    validateToken();
+    
+    $message = trim(filter_input(INPUT_POST, 'message'));
+    $message = $message !== '' ? $message : '...';
+  
+    $fp = fopen(FILENAME, 'a');
+    fwrite($fp, $message . "\n");
+    fclose($fp);
+
+    header('Location: http://localhost:8080/result.php');
+    exit;
+    } 
+
+  $messages = file(FILENAME, FILE_IGNORE_NEW_LINES);
+
   include('../app/_parts/_header.php');
 ?>
 
-<form action ="result.php" method ="get">
-  <select name="colors[]" multiple>
-    <label><input type= "radio" name="color" value = "orange">Orange</label>
-    <label><input type= "radio" name="color" value = "pink">Pink</label>
-    <label><input type= "radio" name="color" value = "gold">gold</label>
-  </select>
-  <button>Send</button>
-  <a href="reset.php">[reset]</a>
+<ul>
+  <?php foreach ($messages as $message): ?>
+  <li><?= h($message); ?></li>
+  <?php endforeach; ?>
+</ul>
+
+<form action ="" method ="post">
+<input type="text" name ="message">  
+<button>Post</button>
+<input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
 </form>
 
 <?php
--- basic_php_webdev-master/work/web/result.php
+++ basic_php_webdev-master/work/web/result.php
@@ -2,15 +2,13 @@
 
   require('../app/functions.php'); 
    /*読み込みに失敗したときに止めてほしいからrequire */
-  
-  $colorFromGet = filter_input(INPUT_GET, 'color') ?? 'transparent';
-  $_SESSION['color'] = $colorFromGet;
 
+  
 
   include('../app/_parts/_header.php');
 ?>
 
-<p><?= h($color); ?></p>
+<p>Message added</p>
 <p><a href="index.php">Go back</a></p>
 
 <?php
